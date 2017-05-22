b2b6ab9418a3b7c2de3b0dfe17fd73a4dc590a12
AndrewPopovich <andrew1994popovich@gmail.com>
2017-05-22 17:57:35 +0300

    Add field UserDao at UserServise & add annotation Autowired

--- MyStockSystem/src/main/java/system/service/UserService.java
+++ MyStockSystem/src/main/java/system/service/UserService.java
@@ -1,4 +1,12 @@
 package system.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import system.DAO.LoginDao;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private LoginDao loginDao;
 }
