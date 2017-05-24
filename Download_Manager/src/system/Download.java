909abf1ee410ec4c56ee91cfdee2acd0b6ea74e6
AndrewPopovich <andrew1994popovich@gmail.com>
2017-05-25 02:24:33 +0300

    Add enumeration Statuses

--- Download_Manager/src/system/Download.java
+++ Download_Manager/src/system/Download.java
@@ -4,5 +4,18 @@ import java.util.Observable;
 
 public class Download extends Observable implements Runnable{
 
+    private static final int BUFFER = 1024;
 
+    public enum Statuses {
+        DOWNLOADING(0), PAUSED(1), COMPLETE(2), CANCELLED(3), ERROR(4);
+
+        Statuses(int i) {
+        }
+    }
+
+
+    @Override
+    public void run() {
+
+    }
 }
