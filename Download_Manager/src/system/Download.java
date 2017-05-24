e0237f0a41b97ab3e3fad9fe87c4b608ed4f4b51
AndrewPopovich <andrew1994popovich@gmail.com>
2017-05-25 02:27:20 +0300

    Add fields at class Download

--- Download_Manager/src/system/Download.java
+++ Download_Manager/src/system/Download.java
@@ -1,10 +1,15 @@
 package system;
 
+import java.net.URL;
 import java.util.Observable;
 
 public class Download extends Observable implements Runnable{
 
     private static final int BUFFER = 1024;
+    private URL url;
+    private long size;
+    private long downloaded;
+    private Statuses status;
 
     public enum Statuses {
         DOWNLOADING(0), PAUSED(1), COMPLETE(2), CANCELLED(3), ERROR(4);
