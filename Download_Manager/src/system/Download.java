e41986812e05548da929aa100292282176467996
AndrewPopovich <andrew1994popovich@gmail.com>
2017-05-25 17:07:03 +0300

    Delete not used imports.

--- Download_Manager/src/system/Download.java
+++ Download_Manager/src/system/Download.java
@@ -2,7 +2,6 @@ package system;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.URL;
