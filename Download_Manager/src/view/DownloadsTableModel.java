1a272148e96b80986fb227463baeb2a00d2a00bd
AndrewPopovich <andrew1994popovich@gmail.com>
2017-05-25 22:31:10 +0300

    Implemented method getRowCount

--- Download_Manager/src/view/DownloadsTableModel.java
+++ Download_Manager/src/view/DownloadsTableModel.java
@@ -24,39 +24,36 @@ public class DownloadsTableModel extends AbstractTableModel implements Observer 
         fireTableRowsInserted(getRowCount() - 1, getRowCount() - 1);
     }
 
-    public Download getDownload(int row){
+    public Download getDownload(int row) {
         return downloadList.get(row);
     }
 
-    public void clearDownload(int row){
+    public void clearDownload(int row) {
         downloadList.remove(row);
 
         fireTableRowsDeleted(row, row);
     }
 
     @Override
-    public int getColumnCount(){
+    public int getColumnCount() {
         return columnNames.length;
     }
 
-    public String getColumnName(int i){
+    public String getColumnName(int i) {
         return columnNames[i];
     }
 
-    public Class getColumnClass(int i){
+    public Class getColumnClass(int i) {
         return columnClasses[i];
     }
 
-
+    public int getRowCount() {
+        return downloadList.size();
+    }
 
     @Override
     public void update(Observable o, Object arg) {
 
-    }
-
-    @Override
-    public int getRowCount() {
-        return 0;
     }
 
     @Override
