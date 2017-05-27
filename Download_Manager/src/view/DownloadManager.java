c6a2a21fb2be2cc65046337ea65e3e49503ee66c
AndrewPopovich <andrew1994popovich@gmail.com>
2017-05-27 15:32:31 +0300

    Create constructor & set title

--- Download_Manager/src/view/DownloadManager.java
+++ Download_Manager/src/view/DownloadManager.java
@@ -3,6 +3,7 @@ package view;
 import system.Download;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -18,6 +19,9 @@ public class DownloadManager extends JFrame implements Observer {
 
     private boolean clearing;
 
+    public DownloadManager() throws HeadlessException {
+        setTitle("Download Manager");
+    }
 
     @Override
     public void update(Observable o, Object arg) {
