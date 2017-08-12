25ea3e37bf08bf7058c8b6460421b9bf29aee09b
AndrewPopovich <andrew1994popovich@gmail.com>
2017-08-12 23:11:01 +0300

    Add game space to repository

---
+++ spacegame/com/andrew/space/Bomb.java
@@ -0,0 +1,17 @@
+package com.andrew.space;
+
+public class Bomb extends BaseObject {
+    public Bomb(double x, double y) {
+        super(x, y, 1);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'B');
+    }
+
+    @Override
+    public void move() {
+        y++;
+    }
+}
